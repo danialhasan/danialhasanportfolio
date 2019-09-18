10827c8998a1257bc21717b11f6ff8cb89440d6b
Danial Hasan <danialhasan14@gmail.com>
2019-09-17 20:35:57 -0400

    pushing uncorrupted files

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 function animateMenuBar(x) {
-	x.classList.toggle("change");
+    x.classList.toggle("change");
 }
 /*
     This function takes the menuBar Id and cycles through its classes. It then toggles the classes to either ".bar", or ".change .bar". 
     The changes occur in ".change .bar"
 */
 var typed = new Typed(".span", {
-	strings: ["Web Developer", "student", "human"],
-	typeSpeed: '8',
-	backSpeed: '9',
-	smartBackspace: true, // Default value
-	loop: true,
-	startDelay: '10',
-	backDelay: '2000',
-	fadeOut: false,
-	showCursor: true
+    strings: ["Web Developer", "student", "human"],
+    typeSpeed: '8',
+    backSpeed: '9',
+    smartBackspace: true, // Default value
+    loop: true,
+    startDelay: '10',
+    backDelay: '2000',
+    fadeOut: false,
+    showCursor: true
 });
 //
 //var insta = document.getElementById("ig");
 //var github = document.getElementById("github");
 //var twitter = document.getElementById("twitter");
+var image = document.getElementsByClassName("flexItem");
+image.addEventListener("click", function () {
+    //    window.location = "https://redreaper752.github.io/restaurant/"
+    alert("hello")
+})
